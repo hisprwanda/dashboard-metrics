177e4a2e41240d0d50800c1f31fb4b50f408d9e9
Derrick-Nuby <iradukundaderrick7@gmail.com>
2025-03-28 09:52:45 +0200

    workflow definitions

--- d2.config.js
+++ d2.config.js
@@ -1,9 +1,11 @@
 const config = {
     type: 'app',
-
+    name: 'Dashboard Usage Metrics',
+    title: 'Dashboard Usage Metrics',
+    description: 'An application to monitor and analyze dashboard usage, developed by HISP Rwanda, featuring an interactive dashboard which shows people who used the app and etc.',
     entryPoints: {
         app: './src/App.tsx',
     },
-}
+};
 
-module.exports = config
+module.exports = config;
